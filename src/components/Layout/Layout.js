0463158e3f340d57454b42efbc53a1aa76c710d1
DrSavauG <dr.savaug@gmail.com>
2022-03-11 17:44:50 +0300

    feat: add Layout.js

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Layout = () => {
+    return (
+        <div>
+            <p>https://github.com/DrSavauG/hw-react-v1.git</p>
+        </div>
+    );
+};
+
+export default Layout;
