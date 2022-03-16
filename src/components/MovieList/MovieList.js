4953540aa26c0ab44353eddac32a62f655947a26
DrSavauG <dr.savaug@gmail.com>
2022-03-16 14:55:08 +0300

    feat: add movie list

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import './style.css'
+import MovieItem from "./MovieItem/MovieItem";
+
+const itemFound = 0;
+const movieLists = [
+    {
+        name: "pulp fiction",
+        year: 2004,
+        genre: 'action & Adventure',
+        url: 'https://resizer.mail.ru/p/f231e9d9-2f9e-5a74-bb77-dff7f31b4eb5/AQACwXo1lp7UpvmbUbLKus0lj0CAGVB9fWSCjPK2hf46UwL298r4aC1cFLq4VUkitsTiW9bgdNE0tZHldKyQ2l4NKTg.jpg'
+    },{
+        name: "bohemian rhapsody",
+        year: 2003,
+        genre: 'Drama,Biograghy,Music',
+        url: 'https://upload.wikimedia.org/wikipedia/be-x-old/2/2e/Bohemian_Rhapsody_poster.png'
+    },
+];
 
 const MovieList = () => {
     return (
-        <div className={'movie-list'}>
-            MovieList
-        </div>
+
+        <>
+            <h3 className={'movie-list-h3'}>{itemFound} movies found</h3>
+            <div className={'movie-list'}>
+                {movieLists.map((el) => (
+                    < MovieItem key={el} data={el}/>
+                ))}
+            </div>
+        </>
+
     );
 };
 
--- src/components/NavigationMain/NavigationMain.js
+++ src/components/NavigationMain/NavigationMain.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style.css';
 
 const NAVIGATION = ['all', 'documentary', 'comedy', "horror", 'crime'];
+
+
 const NavigationMain = () => {
+
     return (
         <ul className={'navigation-main__ul'}>
             {NAVIGATION.map((el) => (
-                <li key={el} className={'navigation-main-ul__li'}>
+                <li key={el} className={`navigation-main-ul__li`}
+                >
                     {el}
                 </li>
             ))}
